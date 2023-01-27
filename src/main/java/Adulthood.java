efd103568d82831dae6af5e8ca5cc98a8b2f1f22
JuliansRecent <juliansrecent@gmail.com>
2023-01-26 20:19:48 -0800

    Module complete. Create a program that prompts the user for their age and conditionally prints different statements if they are 18 or older, or younger

--- src/main/java/Adulthood.java
+++ src/main/java/Adulthood.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 public class Adulthood {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        // Write your program here 
+        // Write your program here
+        // Prompt the user for their age
+        System.out.println("How old are you?");
+        int userAge = Integer.valueOf(scanner.nextLine());
+
+        if (userAge >= 18) {
+            // If they are 18 or older, print "You are an adult."
+            System.out.println("You are an adult.");
+        } else {
+            // If they are younger, print "You are not an adult."
+            System.out.println("You are not an adult.");
+        }
     }
 }
